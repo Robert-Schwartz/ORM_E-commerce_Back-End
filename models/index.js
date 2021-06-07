c0f5ad5ad923071b92090304d1b41656dd8a8ec5
Robert Schwartz <bschwartz23@gmail.com>
2021-06-07 00:52:58 -0700

    add sequelize.sync({force: true}) to server listener '

--- models/index.js
+++ models/index.js
@@ -4,13 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+/* Product belongs to Category,
+as a category can have multiple products
+but a product can only belong to one category.*/;
+Product.belongsTo(Category);
+Category.hasMany(Product);
 
 // Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
+/* Product belongs to many Tag models.
+Using the ProductTag through model,
+allow products to have multiple tags
+and tags to have many products.*/
+Product.belongsToMany(Tag, {
+  through: ProductTag
+})
 
 // Tags belongToMany Products (through ProductTag)
+
+
+
+
 
 module.exports = {
   Product,
